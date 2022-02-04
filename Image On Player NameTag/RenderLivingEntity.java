12a080fbba944cd770fa76492e59841bc1dbbb43
wcaleniekubaa <87438086+wcaleniekubaa@users.noreply.github.com>
2022-02-04 21:48:35 +0100

    Update RenderLivingEntity.java

--- Image On Player NameTag/RenderLivingEntity.java
+++ Image On Player NameTag/RenderLivingEntity.java
@@ -3,3 +3,7 @@ protected boolean canRenderName(T entity)
     // delete "&& entity != this.renderManager.livingPlayer"
     return Minecraft.isGuiEnabled() && entity != this.renderManager.livingPlayer && !entity.isInvisibleToPlayer(entityplayersp) && entity.riddenByEntity == null;
 }
+public void renderName(T entity, double x, double y, double z){
+    // replace world renderer function wait for it
+
+}
